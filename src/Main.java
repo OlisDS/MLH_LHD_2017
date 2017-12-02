5b1c0bb6609494a8f987268f274bdb613db46040
AZ Vasquez <azvasquez99@gmail.com>
2017-12-02 14:19:09 -0500

    created the Initialize()

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ public class Main {
         CommentCounter commentCounter = new CommentCounter();
 
         testInventory();
-
-        CommentAdder adder = new CommentAdder();
-        Asset SingleLine = new Asset(150, 1, 10000000);
-        Asset MultiLine = new Asset(1000, 5, 5000000);
-        Asset JavaDoc = new Asset(11000, 40, 5000000);
-        Asset ReadMe = new Asset(25000, 80, 2500000);
-        Asset HeaderDesignPatterns = new Asset(500000, 200, 2500000);
-        Asset QACommenter = new Asset(1000000, 400, 25000000);
-        Asset AutoScript = new Asset(25000000, 1000, 125000000);
-        Asset ReadableCode = new Asset(50000000, 6666, 10000000);
         
+        Asset[] assetTypes = new Asset[8];
+        
+        initialize(assetTypes)
+        
+        CommentAdder adder = new CommentAdder(assetTypes);
 
         gameLoop:
         while (true) {
@@ -25,5 +20,18 @@ public class Main {
 
     public static void testInventory() {
         //set values of how much of each asset we have
+    }
+    
+    public static void initialize(Asset[] assetTypes){
+        Asset SingleLine = new Asset(150, 1, 10000000);
+        Asset MultiLine = new Asset(1000, 5, 5000000);
+        Asset JavaDoc = new Asset(11000, 40, 5000000);
+        Asset ReadMe = new Asset(25000, 80, 2500000);
+        Asset HeaderDesignPatterns = new Asset(500000, 200, 2500000);
+        Asset QACommenter = new Asset(1000000, 400, 25000000);
+        Asset AutoScript = new Asset(25000000, 1000, 125000000);
+        Asset ReadableCode = new Asset(50000000, 6666, 10000000);
+        assetTypes = {SingleLine, MultiLine, JavaDoc, ReadMe, HeaderDesignPatterns, QACommenter, AutoScript, ReadableCode};
+    
     }
 }
