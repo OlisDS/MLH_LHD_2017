1d4604dfdc3e34f7a3215cb0b363e3827c7a1158
joeylu38 <joeylu@ufl.edu>
2017-12-02 11:58:16 -0500

    gui crap

---
+++ src/commentcutter.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class commentcutter {
+
+    public static void main(String[] args) {
+        JFrame frame =  new JFrame();
+        frame.setSize(400, 300);
+        frame.setTitle("Comment Cutter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
